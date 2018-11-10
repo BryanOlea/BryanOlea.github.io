09b3534adb010f6831e55a8f57bd1058ba5c2f7e
BryanOlea <BryanOlea@users.noreply.github.com>
2018-11-09 20:23:57 -0600

    sdsd

---
+++ ASySPrac3/Practica3.m
@@ -0,0 +1,11 @@
+%% Práctica 3: Señales en tiempo discreto
+% Caudillo Barbosa Eric
+% Olea García Bryan
+%% Objetivos
+% 
+% * Manipulación básica de MATLAB
+% * Gráficas de señales reales y complejas discretas
+% * Transformación de señales discretas (escalamientos y traslaciones)
+% * Calculo de energía y potencia de señales discretas
+% 
+
